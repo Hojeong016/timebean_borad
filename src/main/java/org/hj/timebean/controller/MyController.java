72aba787d86d50f77570a663d5f8e3ed356bed09
Hogeong016 <skflsehdn@naver.com>
2024-09-17 02:15:14 +0900

    Home,My,Dm 화면 설계

--- src/main/java/org/hj/timebean/controller/MyController.java
+++ src/main/java/org/hj/timebean/controller/MyController.java
@@ -1,4 +1,14 @@
 package org.hj.timebean.controller;
 
-public class Mycontroller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/my") //my/{accountId}"
+    public String myPage(/*@PathVariable("accountId") String accountId*/) {
+        return "main/my";
+    }
 }
